8318f92ddd4ddb480987a6c53cf2478197770a25
Johnathan Simeroth <john.simeroth@gmail.com>
2023-10-20 09:24:09 +0100

    reproduced bug

---
+++ packages/ui/src/Pre.tsx
@@ -0,0 +1,10 @@
+import { styled, SizableText } from 'tamagui'
+
+export const Pre = styled(SizableText, {
+  name: 'Pre',
+  theme: 'dark_green',
+  color: '$color9',
+  backgroundColor: '$gray3',
+  numberOfLines: 0,
+  tag: 'pre',
+})
